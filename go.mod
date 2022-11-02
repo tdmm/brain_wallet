bd552ca2fd3d20bd1bba355d1d59b2944cef73d3
Bogang Wang <bogangwang@192.168.0.2>
2022-11-02 12:37:37 +0800

    update

--- go.mod
+++ go.mod
@@ -1,7 +1,15 @@
 module github.com/brain_wallet
 
-go 1.17
+go 1.18
 
 require github.com/tyler-smith/go-bip39 v1.1.0
 
-require golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+require (
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
+	github.com/ethereum/go-ethereum v1.10.4 // indirect
+	github.com/miguelmota/go-ethereum-hdwallet v0.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
+
--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 	"github.com/tyler-smith/go-bip39"
 )
 
+var start = flag.Int64("s", 1, "ethereum hd index start ")
+var end = flag.Int64("e", 1, "ethereum hd index end ")
+
+const ETHEREUM_HD_PATH = "m/44'/60'/0'/0/"
+
 func main() {
-	entropy, err := getPassword()
+	flag.Parse()
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter entropy: ")
+	entropy, err := reader.ReadString('\n')
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
+		return
 	}
 
 	if len(entropy) < 16 {
@@ -26,17 +37,30 @@ func main() {
 		return
 	}
 
-	fmt.Println(mnemonic)
-}
+	fmt.Printf("entropy: %s \nmnemonic: %s\n", []byte(entropy)[:16], mnemonic)
 
-func getPassword() (string, error) {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter entropy: ")
-	entropy, err := reader.ReadString('\n')
+	hdWallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	if err != nil {
-		return "", err
+		fmt.Printf("NewFromMnemonic err:%s", err.Error())
+		return
 	}
 
-	return strings.TrimSpace(entropy), nil
+	for i := *start; i <= *end; i++ {
+		path := ETHEREUM_HD_PATH + fmt.Sprintf("%d", i)
+		drPath, err := hdwallet.ParseDerivationPath(path)
+		if err != nil {
+			panic(err)
+		}
+
+		account, err := hdWallet.Derive(drPath, false)
+		if err != nil {
+			panic(err)
+		}
+		priv, err := hdWallet.PrivateKeyHex(account)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%s: %s 0x%s\n", path, account.Address.String(), priv)
+	}
 }
